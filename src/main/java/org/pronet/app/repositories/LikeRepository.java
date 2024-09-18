2ae5961db893c816d5aa20e077608fc198f64027
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-09-18 17:00:40 +0400

    Create methods

--- src/main/java/org/pronet/app/repositories/LikeRepository.java
+++ src/main/java/org/pronet/app/repositories/LikeRepository.java
@@ -4,6 +4,11 @@ import org.pronet.app.entities.Like;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface LikeRepository extends JpaRepository<Like, Long> {
+    List<Like> findAllByUserIdAndPostId(Long userId, Long postId);
+    List<Like> findAllByUserId(Long userId);
+    List<Like> findAllByPostId(Long postId);
 }
