416e04192b2a5e13de1f988e8b89e4316ee969ae
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-09-18 12:34:50 +0400

    Create repository: PostRepository

---
+++ src/main/java/org/pronet/app/repositories/PostRepository.java
@@ -0,0 +1,9 @@
+package org.pronet.app.repositories;
+
+import org.pronet.app.entities.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
