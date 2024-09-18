b539a6f2188fba941359ea85c20a3453456f35f7
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-09-18 14:29:55 +0400

    Create controller: PostController

---
+++ src/main/java/org/pronet/app/controllers/PostController.java
@@ -0,0 +1,45 @@
+package org.pronet.app.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.pronet.app.entities.Post;
+import org.pronet.app.requests.post.PostCreateRequest;
+import org.pronet.app.requests.post.PostUpdateRequest;
+import org.pronet.app.services.PostService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(value = "/post")
+public class PostController {
+    private final PostService postService;
+
+    @PostMapping(value = "/create")
+    public Post createPost(@RequestBody PostCreateRequest request) {
+        return postService.createPost(request);
+    }
+
+    @GetMapping(value = "/list")
+    public List<Post> getPostList(@RequestParam Optional<Long> userId) {
+        return postService.getPostList(userId);
+    }
+
+    @GetMapping(value = "/details/{postId}")
+    public Post getPostById(@PathVariable(value = "postId") Long postId) {
+        return postService.getPostById(postId);
+    }
+
+    @PutMapping(value = "/update/{postId}")
+    public Post updatePost(
+            @PathVariable(value = "postId") Long postId,
+            @RequestBody PostUpdateRequest request) {
+        return postService.updatePost(postId, request);
+    }
+
+    @DeleteMapping(value = "/delete/{postId}")
+    public void deletePost(@PathVariable(value = "postId") Long postId) {
+        postService.deletePost(postId);
+    }
+}
