5736c758e977fe8d003c44d8faaa880bab59b73d
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-09-18 16:42:06 +0400

    Create methods

--- src/main/java/org/pronet/app/repositories/CommentRepository.java
+++ src/main/java/org/pronet/app/repositories/CommentRepository.java
@@ -4,6 +4,11 @@ import org.pronet.app.entities.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findAllByUserIdAndPostId(Long userId, Long postId);
+    List<Comment> findAllByUserId(Long userId);
+    List<Comment> findAllByPostId(Long postId);
 }
