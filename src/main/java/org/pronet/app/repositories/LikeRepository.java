dd62da064eb84106f2f29f4730dde70fa8a9721f
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-09-18 12:36:36 +0400

    Create repository: LikeRepository

---
+++ src/main/java/org/pronet/app/repositories/LikeRepository.java
@@ -0,0 +1,9 @@
+package org.pronet.app.repositories;
+
+import org.pronet.app.entities.Like;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LikeRepository extends JpaRepository<Like, Long> {
+}
