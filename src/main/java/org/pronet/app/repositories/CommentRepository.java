a312ccbf238283c532c08b731433260569e975a2
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-09-18 12:36:19 +0400

    Create repository: CommentRepository

---
+++ src/main/java/org/pronet/app/repositories/CommentRepository.java
@@ -0,0 +1,9 @@
+package org.pronet.app.repositories;
+
+import org.pronet.app.entities.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+}
