33d1cdecd523b37aa27d42ae8ae3952546df8a7c
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-09-20 15:51:25 +0400

    Change annotation

--- src/main/java/org/pronet/app/security/JwtAuthenticationFilter.java
+++ src/main/java/org/pronet/app/security/JwtAuthenticationFilter.java
@@ -4,8 +4,8 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
 import org.pronet.app.services.implementations.UserDetailsServiceImplementation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -15,10 +15,12 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-@RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-    private final JwtTokenProvider jwtTokenProvider;
-    private final UserDetailsServiceImplementation userDetailsServiceImplementation;
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+    @Autowired
+    private UserDetailsServiceImplementation userDetailsServiceImplementation;
 
     @Override
     protected void doFilterInternal(
@@ -43,8 +45,9 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private String extractTokenFromRequest(HttpServletRequest request) {
         String bearer = request.getHeader("Authorization");
-        if(StringUtils.hasText(bearer) && bearer.startsWith("Bearer "))
+        if (StringUtils.hasText(bearer) && bearer.startsWith("Bearer ")) {
             return bearer.substring("Bearer".length() + 1);
+        }
         return null;
     }
 }
