93d60b372e003a6f0b7629453a57b5fc7f4b58a5
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-09-23 20:11:26 +0400

    Remove request matcher list

--- src/main/java/org/pronet/app/configurations/AuthenticationRequestMatcher.java
+++ src/main/java/org/pronet/app/configurations/AuthenticationRequestMatcher.java
@@ -4,21 +4,8 @@ public class AuthenticationRequestMatcher {
     public static final String[] NON_AUTH_ROUTES = {
             "/post/list",
             "/comment/list",
+            "/like/list",
             "/auth/register",
             "/auth/login"
-    };
-
-    public static final String[] USER_AUTH_ROUTES = {
-            "/post/create",
-            "/post/details/{postId}",
-            "/post/update/{postId}",
-            "/post/delete/{postId}",
-            "/comment/create",
-            "/comment/details/{commentId}",
-            "/comment/update/{commentId}",
-            "/comment/delete/{commentId}",
-            "/like/create",
-            "/like/details/{likeId}",
-            "/like/delete/{likeId}",
     };
 }
