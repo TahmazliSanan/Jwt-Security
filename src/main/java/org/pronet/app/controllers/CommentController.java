5817cf1c7be927c3d89737a934f3acd07f808892
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-09-23 12:07:57 +0400

    Change return type of methods

--- src/main/java/org/pronet/app/controllers/CommentController.java
+++ src/main/java/org/pronet/app/controllers/CommentController.java
@@ -1,9 +1,9 @@
 package org.pronet.app.controllers;
 
 import lombok.RequiredArgsConstructor;
-import org.pronet.app.entities.Comment;
 import org.pronet.app.requests.comment.CommentCreateRequest;
 import org.pronet.app.requests.comment.CommentUpdateRequest;
+import org.pronet.app.responses.CommentResponse;
 import org.pronet.app.services.CommentService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,22 +19,22 @@ public class CommentController {
     private final CommentService commentService;
 
     @PostMapping(value = "/create")
-    public ResponseEntity<Comment> createComment(@RequestBody CommentCreateRequest request) {
-        Comment createdComment = commentService.createComment(request);
+    public ResponseEntity<CommentResponse> createComment(@RequestBody CommentCreateRequest request) {
+        CommentResponse createdComment = commentService.createComment(request);
         return new ResponseEntity<>(createdComment, HttpStatus.CREATED);
     }
 
     @GetMapping(value = "/list")
-    public ResponseEntity<List<Comment>> getCommentList(
+    public ResponseEntity<List<CommentResponse>> getCommentList(
             @RequestParam Optional<Long> userId,
             @RequestParam Optional<Long> postId) {
-        List<Comment> commentList = commentService.getCommentList(userId, postId);
+        List<CommentResponse> commentList = commentService.getCommentList(userId, postId);
         return new ResponseEntity<>(commentList, HttpStatus.OK);
     }
 
     @GetMapping(value = "/details/{commentId}")
-    public ResponseEntity<Comment> getCommentById(@PathVariable(value = "commentId") Long commentId) {
-        Comment foundedComment = commentService.getCommentById(commentId);
+    public ResponseEntity<CommentResponse> getCommentById(@PathVariable(value = "commentId") Long commentId) {
+        CommentResponse foundedComment = commentService.getCommentById(commentId);
         if (foundedComment != null) {
             return new ResponseEntity<>(foundedComment, HttpStatus.OK);
         }
@@ -42,12 +42,12 @@ public class CommentController {
     }
 
     @PutMapping(value = "/update/{commentId}")
-    public ResponseEntity<Comment> updateComment(
+    public ResponseEntity<CommentResponse> updateComment(
             @PathVariable(value = "commentId") Long commentId,
             @RequestBody CommentUpdateRequest request) {
         Boolean existCommentById = commentService.existsById(commentId);
         if (existCommentById) {
-            Comment updatedComment = commentService.updateComment(commentId, request);
+            CommentResponse updatedComment = commentService.updateComment(commentId, request);
             return new ResponseEntity<>(updatedComment, HttpStatus.OK);
         }
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
